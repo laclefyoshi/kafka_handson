6e28cbc6bdf40af2a286e0cf73520a2243131a63
Yoshiyasu Saeki <441570+laclefyoshi@users.noreply.github.com>
2020-10-25 14:44:27 +0900

    add message about assigned partitions

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
 public class Consumer {
     private KafkaConsumer<String, String> c;
@@ -20,6 +21,7 @@ public class Consumer {
             props.load(new FileInputStream(propertiesFilePath));
         } catch (IOException e) {
             System.err.println("Failed to load " + propertiesFilePath);
+            System.exit(1);
         }
         c = new KafkaConsumer<>(props);
         assignTopic();
@@ -32,15 +34,20 @@ public class Consumer {
     public void run() {
         try {
             while (true) {
+                for (TopicPartition tp: c.assignment()) {
+                    System.out.printf("\ttopic = %s, partition = %d\n",
+                        tp.topic(), tp.partition());
+                }
                 ConsumerRecords<String, String> records = c.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, String> record : records) {
                     System.out.printf("offset = %d, key = %s, value = %s\n",
                         record.offset(), record.key(), record.value());
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             c.close();
         }
-        
     }
 }
